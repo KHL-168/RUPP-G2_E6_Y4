9e19b216bc3697b0af20c37feecc116f72351306
Kim Horlong <k.horlong169@gmail.com>
2024-12-06 10:37:32 +0700

    Your commit message

---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    // screens: {
+      
+    // },
+    extend: {
+      fontFamily: {
+        'Koulen':['Koulen', 'sans-serif'],
+        'Poppins':['Poppins', 'sans-serif'],
+      },
+
+    },
+  },
+  plugins: [],
+}
